49c0886fadabf0134e63e256e9bd5b3a8efdc30f
jemi14 <griselda_gsis@hotmail.com>
2023-06-08 14:38:49 -0300

    Clase 1

---
+++ ejemplos_videos/src/main/java/com/example/ejemplos_videos/controllers/PrimerPruebaControlador.java
@@ -0,0 +1,18 @@
+package com.example.ejemplos_videos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/primerPrueba")
+
+public class PrimerPruebaControlador {
+	
+	@GetMapping("iniciando")
+	public String iniciandoElCurso() {
+		return "IniciandoPagina";
+		
+	}
+
+}
